d28d03bcec10394cc03890e3072808e80b20f2f0
Iulian Asanache <costi-iulian.asanache@epita.fr>
2026-02-28 01:53:57 +0100

    chore: Bump version to 1.1.0

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// User CRUD routes
 router.post('/', userController.create.bind(userController));
 router.get('/', userController.getAll.bind(userController));
 router.get('/:id', userController.getById.bind(userController));
